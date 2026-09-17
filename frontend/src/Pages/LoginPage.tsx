d213ec167010db76c57fd91b1f8750f0f79817da
jon0riginal <g.antonym497@passmail.com>
2026-09-17 16:00:07 +1200

    feat: add authentication check and redirect to dashboard in LoginPage

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -1,6 +1,13 @@
 import LoginForm from '../Components/LoginForm';
+import { getAuthentication } from '../util.ts';
+import { useNavigate } from 'react-router';
 
 export default function LoginPage() {
+  const authentication = getAuthentication();
+  const navigator = useNavigate();
+  if (authentication) {
+    navigator('/dashboard');
+  }
   return (
     <div className="flex-1 flex items-center justify-center px-4 py-8">
       <LoginForm />
